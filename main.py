d23b32c9ba4b023c7e672e010e22beeddec30e32
Tobias Schwackenhofer <tobiasschw@gmail.com>
2018-10-12 00:19:28 +0200

    Add config loading.

--- main.py
+++ main.py
@@ -5,6 +5,16 @@ from routes import pages
 app = Flask(__name__)
 app.register_blueprint(pages)
 
+app.config.update(
+    DEBUG=True,
+    APPLICATION_ROOT="/",
+    SECRET_KEY=b"TheNotSoSecretDevKey"
+)
+
+# Overwrite default dev settings with production settings.
+# You must make sure this file is *not* checked into git!
+app.config.from_pyfile("config.cfg", silent=True)
+
 
 @app.errorhandler(404)
 def page_not_found(err):
